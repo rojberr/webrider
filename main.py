617aaeb1a73ef573e7dbbc19e4e0d46aab35ea56
jakubdrzymala <jakub.drzymala@deutschebahn.com>
2023-05-13 22:52:19 +0200

    Upgrade

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import time
+
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
@@ -10,14 +15,26 @@ if __name__ == '__main__':
     print_hi('PyCharm')
 
     # Set up the remote webdriver
-    driver = webdriver.Remote(
-        command_executor='http://localhost:4444/wd/hub',
-        desired_capabilities={'browserName': 'chrome'}
-    )
+    #driver = webdriver.Remote(
+        #command_executor='http://0.0.0.0:4444/wd/hub',
+        #options=chromeOptions
+    #)
+    chrome_options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome(ChromeDriverManager().install())
 
-    driver.get('https://www.google.com')
+    #version = driver.execute_script('return (window.chrome || {}).webdriver_version || "N/A"')
+    #print("ChromeDriver version: ", version)
 
-    driver.implicitly_wait(99999999999999999999)
+    driver.get('https://api.pekabex.pl')
+
+    button = driver.find_element(By.XPATH, "/html/body/main/div/div[2]/div/form[1]/button")
+    button.click()
+
+    driver.implicitly_wait(5)
+    inputElement = driver.find_element(By.ID, 'i0116')
+    inputElement.send_keys('dupa jas')
+
+    time.sleep(90)
 
     driver.quit()
 
